319b0c9b1cd417df4a02763fa0ebc6fdf211815d
Alexandre Rocha <alexvr25@gmail.com>
2024-11-05 21:37:16 +0000

    fix

--- DataStructs/Nodes/BinaryTreeNode.java
+++ DataStructs/Nodes/BinaryTreeNode.java
@@ -14,14 +14,9 @@ public class BinaryTreeNode<T> {
      * The left child of this node.
      */
     private BinaryTreeNode<T> left;
-<<<<<<< HEAD
-    /**
-     * 
-=======
-
+    
     /**
      * The right child of this node.
->>>>>>> 5bb1e3f2f8220477816c56019a90d5fc2e5badad
      */
     private BinaryTreeNode<T> right;
 
