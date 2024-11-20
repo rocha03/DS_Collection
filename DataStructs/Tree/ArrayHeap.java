bee7f6fe163de587f3afcf4c40c663ae53192ef2
Arneiro <joao.a.carneiro@gmail.com>
2024-11-20 12:00:46 +0000

    JoaoCarneiroLindo

---
+++ DataStructs/Tree/ArrayHeap.java
@@ -0,0 +1,13 @@
+package DataStructs.Tree;
+
+import Interfaces.Tree.HeapADT;
+
+/**
+ * ArrayHeap provides an array implementation of a minheap.
+ *
+ */
+public class ArrayHeap<T> extends ArrayBinaryTree<T> implements HeapADT<T> {
+    public ArrayHeap() {
+        super();
+    }
+}
